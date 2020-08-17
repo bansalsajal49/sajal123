65c76c1ebe2c03d0b363bec175a15ce677393086
bansalsajal49 <bansalsajal49@gmail.com>
2020-08-17 19:23:26 +0530

    commit

--- software.py
+++ software.py
@@ -4,4 +4,4 @@ pyttsx3.speak("Hello, Welcome to the Stimulation")
 pyttsx3.speak("what would you like me to do")
 x=input()
 if ("open" in x or "run" in x or "launch" in x) and ("notepad" in x or "notebook" in x):
-	os.system("notepad")
+	os.system("chrome")
