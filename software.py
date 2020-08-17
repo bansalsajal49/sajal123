5e8ff499273d3a4bbffb8ffd8291a6732c370a40
bansalsajal49 <bansalsajal49@gmail.com>
2020-08-17 19:20:16 +0530

    my file

---
+++ software.py
@@ -0,0 +1,7 @@
+import pyttsx3
+import os
+pyttsx3.speak("Hello, Welcome to the Stimulation")
+pyttsx3.speak("what would you like me to do")
+x=input()
+if ("open" in x or "run" in x or "launch" in x) and ("notepad" in x or "notebook" in x):
+	os.system("notepad")
